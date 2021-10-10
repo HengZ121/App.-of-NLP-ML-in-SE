7af1baa910a9d78330a716dcd8c9887e0942d36c
HengZ121 <hzhan274@uOttawa.ca>
2021-10-09 21:21:00 -0400

    F1 to F3 complete

--- assignment 2/Sentence.java
+++ assignment 2/Sentence.java
@@ -3,6 +3,8 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import edu.stanford.nlp.ling.*;
 import edu.stanford.nlp.pipeline.*;
 import edu.stanford.nlp.simple.*;
@@ -29,26 +31,37 @@ public class Sentence {
      */
     public Sentence(CoreDocument sentence){
         this.sentence = sentence;
-        this.f1 = getF1();
+        int[] feature_1_to_3 = getF1_3();
+        this.f1 = feature_1_to_3[0];
+        this.f2 = feature_1_to_3[1];
+        this.f3 = feature_1_to_3[2];
     }
 
-    /***  
+    /***
+    * Feature 1 to 3
     * Get the position of "it" in the sentence considering the number of tokens
-    * @return int   The position of "it" in the sentence; return -1 if no "it" is found
-    * @version 1.0: comma or other punctuation marks is also assigned a token
+    * Get the length of the sentence in terms of tokens
+    * Get the number of punctuations
+    * @return int[5]   res[0] = -1 if no "it" in sentence; res[1] = -1 if sentence is invalid
+    * @version 2.0
     */
-    private int getF1(){
-        int pos = 0; ///sentence[0] indicates Class of sentence and has value either NomAnaph or ClauseAnaph
+    private int[] getF1_3(){
+        int[] res = {-1,-1,0};
+        int counter = 0;
+        int punctuations = 0;
+        
         for (CoreLabel tok : this.sentence.tokens()) {
             if (tok.word().toLowerCase().equals("it")){
-                return pos;
-            }else{
-                pos ++;
+                res[0] = counter; /// Position of "it" F1 done
+            }else if (Pattern.matches("\\p{Punct}", tok.word()) || tok.word().equals("...")){/// Use Pattern to check punctuations
+                punctuations ++;
             }
+            counter ++;
         }
-        return -1;
+        res[1] = counter - 1; /// F2 done
+        res[2] = punctuations; // F3 done
+        return res;
     }
-
 
     /*** 
     * Main Function
@@ -79,7 +92,7 @@ public class Sentence {
             CoreDocument to_be_tokenized = new CoreDocument(elem);
             pipeline.annotate(to_be_tokenized); /// Sentences Tokenized 
             Sentence sentence = new Sentence(to_be_tokenized);
-            System.out.println(elem + " " + sentence.f1); //// Testing
+            System.out.println(elem + " " + sentence.f1+ " " + sentence.f2+ " " + sentence.f3); //// Testing
         }
     }
 }
